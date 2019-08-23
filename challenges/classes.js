3781c322926e69b7d596889a06395f62c9494297
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-08-23 10:58:54 -0700

    some stretch complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,24 +1,46 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidClass{
-    constructor(attributes){
+class CuboidClass {
+  constructor(attributes) {
     this.length = attributes.length;
     this.width = attributes.width;
     this.height = attributes.height;
   }
-  volume(){
+  volume() {
     return this.length * this.height * this.width;
   }
-  surfaceArea(){
-    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  surfaceArea() {
+    return (
+      2 *
+      (this.length * this.width +
+        this.length * this.height +
+        this.width * this.height)
+    );
   }
 }
 
-class CubeMaker extends CuboidClass{
-    constructor(attributes){
-        
-    }
+class CubeMaker extends CuboidClass {
+  constructor(attributes) {
+    super(attributes);
+  }
+  cubeSurf() {
+    return 6 * (Math.pow(this.length, 2));
+  }
+  cubeVol() {
+    return (Math.pow(this.length, 3));
+  }
 }
+const cube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5
+});
+
+
+console.log("** classes stretch start")
+console.log(cube.cubeSurf()); 
+console.log(cube.cubeVol()); 
+console.log("** classes stretch end");
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -62,6 +62,14 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+// const universities = [];
+// graduates.forEach(function(element){
+//   universities.push(element.university);
+//   universities.sort();
+//   return universities;
+// });
+// console.log(universities);
+
 const universities = [];
 graduates.forEach(function(element){
   universities.push(element.university);
